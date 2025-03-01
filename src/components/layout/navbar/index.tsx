10fd66bc16ac2de815f12d197609c3eb9e95303b
Rafi Pandya <rafipandya@gmail.com>
2025-03-01 22:08:16 +0700

    - Commit 1 March 2025 1. Start the app 2. Removing unused codes and component 3. Install requirement dependencies 4. Restructuring application

---
+++ src/components/layout/navbar/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState } from "react";
+import "@/styles/components/_navbar.scss"
+import Link from "next/link";
+
+
+const Navbar = () => {
+    return (
+        <></>
+    )
+}
+
+export default Navbar;
